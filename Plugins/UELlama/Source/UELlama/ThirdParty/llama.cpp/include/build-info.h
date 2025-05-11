f1790cda5464a90c4488483f81f821a47364e37f
burtsloane1 <burtsloane@gmail.com>
2025-05-10 19:03:34 -0700

    removing old llama.cpp

--- Plugins/UELlama/Source/UELlama/ThirdParty/llama.cpp/include/build-info.h
+++
@@ -1,9 +0,0 @@
-#ifndef BUILD_INFO_H
-#define BUILD_INFO_H
-
-#define BUILD_NUMBER 1258
-#define BUILD_COMMIT "a40f2b65"
-#define BUILD_COMPILER "Apple clang version 15.0.0 (clang-1500.3.9.4)"
-#define BUILD_TARGET "arm64-apple-darwin23.3.0"
-
-#endif // BUILD_INFO_H
